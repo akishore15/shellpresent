9e0b8d2efb7165bd4461b1a18a85bf21ecfc29a6
hitor- 1 <154870205+akishore15@users.noreply.github.com>
2025-01-19 01:35:56 -0800

    Add files via upload

---
+++ app.svelte
@@ -0,0 +1,16 @@
+<!-- App.svelte -->
+<script>
+  import { onMount } from "svelte";
+  import Slide from "./Slide.svelte";
+
+  let slides = [];
+
+  onMount(async () => {
+      const response = await fetch("http://localhost:8080/api/slides");
+      slides = await response.json();
+  });
+</script>
+
+{#each slides as slide}
+  <Slide content={slide}/>
+{/each}
